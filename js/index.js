abdca9e465cb040970082f32934ef727fba09bd1
NastiaBondarenko <nastiabondarenko17@gmail.com>
2020-10-29 23:36:48 +0200

    fix for validation

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 const nameId = ['women', 'men', 'child', 'brend', 'company'];
 const buttId = ["womenButt", "menButt", "chidButt", "brendButt", "companyButt"]; 
 
-document.getElementById('women').hidden = true;
+
 
 const change = (ids) =>{
 	for(let i = 0; i < nameId.length; i++){
